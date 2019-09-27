6f12d7fc76d199c489faf50eca81cd403e9c17e2
Jonathan Garvey <jon.d.garvey@gmail.com>
2019-09-27 14:45:18 -0700

    checking in basic terraform infra

---
+++ example.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  profile = "default"
+  region  = var.region
+}
+
+resource "aws_instance" "example" {
+  ami           = var.amis[var.region]
+  instance_type = "t2.micro"
+  tags = {
+    name = "Hello Example"
+  }
+}
+
+resource "aws_eip" "ip" {
+  vpc      = true
+  instance = aws_instance.example.id
+}
